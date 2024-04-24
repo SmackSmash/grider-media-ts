8f8ad853877e9119734dc253f7d7c98da08abb18
Dan Moffat <danmoffat@gmail.com>
2024-04-24 13:24:30 +0100

    Converted Panel and Button components to TS

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
+import Panel from './components/Panel';
+
 const App = () => {
   return (
     <main className='p-4'>
       <h1>App works</h1>
+      <Panel className='text-black'>Some panel content</Panel>
     </main>
   );
 };
---
+++ src/components/Panel.tsx
@@ -0,0 +1,21 @@
+import { type PropsWithChildren } from 'react';
+import classNames from 'classnames';
+
+type PanelProps = PropsWithChildren<{
+  className?: string;
+}>;
+
+function Panel({ children, className, ...rest }: PanelProps) {
+  const finalClassNames = classNames(
+    'border rounded p-3 shadow bg-white w-full',
+    className
+  );
+
+  return (
+    <div className={finalClassNames} {...rest}>
+      {children}
+    </div>
+  );
+}
+
+export default Panel;
