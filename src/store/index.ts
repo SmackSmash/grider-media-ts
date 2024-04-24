f2ab0b633084afabd86d06cbbff7bcb1052665fe
Dan Moffat <danmoffat@gmail.com>
2024-04-24 13:45:28 +0100

    Moved default exports to bottom of file

--- src/store/index.ts
+++ src/store/index.ts
@@ -10,8 +10,8 @@ const store = configureStore({
   reducer: { users, albums, photos }
 });
 
-export default store;
-
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
 export * from './hooks';
+
+export default store;
