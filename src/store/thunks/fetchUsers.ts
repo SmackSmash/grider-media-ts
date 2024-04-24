3afaf6148ea79419991d020068030cd2334bf28b
Dan Moffat <danmoffat@gmail.com>
2024-04-24 17:54:51 +0100

    Added delay function that returns a promise to simulate slow connections

--- src/store/thunks/fetchUsers.ts
+++ src/store/thunks/fetchUsers.ts
@@ -1,8 +1,9 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
+import { delay } from '../../utils';
 
 export const fetchUsers = createAsyncThunk('users/fetch', async () => {
   const response = await axios.get('http://localhost:3000/users');
-  console.log(response);
+  await delay(5000);
   return response.data;
 });
