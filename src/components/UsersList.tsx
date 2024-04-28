c261c3c475721178b9c9cd6f3be05b15480e9419
Dan Moffat <danmoffat@gmail.com>
2024-04-28 21:29:28 +0100

    Show list of users on UserList page

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -1,15 +1,24 @@
 import { useEffect } from 'react';
 import { fetchUsers } from '../store';
-import { useAppDispatch } from '../store';
+import { useAppDispatch, useAppSelector } from '../store';
 
 const UsersList = () => {
+  const users = useAppSelector(({ users: { data } }) => data);
   const dispatch = useAppDispatch();
+
+  console.log(users);
 
   useEffect(() => {
     dispatch(fetchUsers());
   }, [dispatch]);
 
-  return <h1>UsersList Works</h1>;
+  return (
+    <div>
+      {users.length
+        ? users.map(({ name, id }) => <h1 key={id}>{name}</h1>)
+        : 'No users yet'}
+    </div>
+  );
 };
 
 export default UsersList;
