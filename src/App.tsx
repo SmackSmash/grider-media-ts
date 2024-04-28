0b2df1c0feb495271517b05e4d6951f1cd215176
Dan Moffat <danmoffat@gmail.com>
2024-04-28 21:17:51 +0100

    Removed fetching logic from App

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,6 @@
-import { useEffect } from 'react';
 import UsersList from './components/UsersList';
-import { fetchUsers } from './store';
-import { useAppDispatch } from './store';
 
 const App = () => {
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    dispatch(fetchUsers());
-  }, [dispatch]);
-
   return (
     <main className='p-4 container mx-auto'>
       <UsersList />
