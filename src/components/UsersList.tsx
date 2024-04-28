fcc1e803343e44de9162a41d82ce7d407e7d0790
Dan Moffat <danmoffat@gmail.com>
2024-04-28 21:20:15 +0100

    Added fetching logic to the UserList

--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -1,4 +1,14 @@
+import { useEffect } from 'react';
+import { fetchUsers } from '../store';
+import { useAppDispatch } from '../store';
+
 const UsersList = () => {
+  const dispatch = useAppDispatch();
+
+  useEffect(() => {
+    dispatch(fetchUsers());
+  }, [dispatch]);
+
   return <h1>UsersList Works</h1>;
 };
 
