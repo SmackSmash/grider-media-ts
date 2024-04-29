51f68d45ecc0c42fbe896854b810c9b1442c3028
Dan Moffat <danmoffat@gmail.com>
2024-04-29 20:16:53 +0100

    Removed rounded corners

--- src/components/Skeleton.tsx
+++ src/components/Skeleton.tsx
@@ -9,7 +9,7 @@ const Skeleton = ({
     .fill(0)
     .map((_, i) => (
       <div
-        className={`${className} bg-slate-800 overflow-hidden rounded relative`}
+        className={`${className} bg-slate-800 overflow-hidden relative`}
         key={i}>
         <div className='inset-0 absolute -translate-x-full bg-gradient-to-r from-slate-800 via-slate-700 to-slate-800 animate-shimmer'></div>
       </div>
--- src/components/UsersList.tsx
+++ src/components/UsersList.tsx
@@ -15,7 +15,7 @@ const UsersList = () => {
   }, [dispatch]);
 
   if (isLoading) {
-    return <Skeleton times={5} className='h-10 w-full m-2' />;
+    return <Skeleton times={5} className='h-10 w-full my-2' />;
   }
 
   if (error) {
