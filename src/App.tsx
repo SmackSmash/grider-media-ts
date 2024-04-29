58d9b3a896fb78eb7bf59c7dcd788bf7a268e41b
Dan Moffat <danmoffat@gmail.com>
2024-04-29 18:19:33 +0100

    Partially updated button to poimandres colors

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,10 @@
 import UsersList from './components/UsersList';
+import Button from './components/Button';
 
 const App = () => {
   return (
     <main className='p-4 container mx-auto'>
+      <Button primary>Hello there</Button>
       <UsersList />
     </main>
   );
