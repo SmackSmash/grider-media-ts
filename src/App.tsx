6180e26b65df6a96859665cd6ab8399448549b3e
Dan Moffat <danmoffat@gmail.com>
2024-04-29 21:38:35 +0100

    Finished implementing addUser button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,20 @@
 import UsersList from './components/UsersList';
 import Button from './components/Button';
+import { useAppDispatch } from './store';
+import { addUser } from './store';
 
 const App = () => {
+  const dispatch = useAppDispatch();
+
+  const handleClick = () => {
+    dispatch(addUser());
+  };
+
   return (
     <main className='p-4 container mx-auto'>
-      <Button primary>Add user</Button>
+      <Button primary onClick={handleClick}>
+        Add user
+      </Button>
       <UsersList />
     </main>
   );
