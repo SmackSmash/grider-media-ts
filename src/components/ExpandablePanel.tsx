9de8c7b546fa0f1e6e510fc9a23bdcaf53caae4b
Dan Moffat <danmoffat@gmail.com>
2024-05-01 19:24:10 +0100

    Experimenting with headlessui transitions

--- src/components/ExpandablePanel.tsx
+++ src/components/ExpandablePanel.tsx
@@ -1,4 +1,5 @@
 import { useState, type PropsWithChildren, type ReactNode } from "react";
+import { Transition } from "@headlessui/react";
 import { FaAngleDown, FaAngleLeft } from "react-icons/fa";
 
 type ExpandablePanelProps = PropsWithChildren<{
@@ -13,9 +14,9 @@ const ExpandablePanel = ({ header, children }: ExpandablePanelProps) => {
   };
 
   return (
-    <div className="mb-4 border border-poimandres-midslate">
+    <div className="mb-4">
       <div
-        className="flex h-10 w-full flex-row items-center pl-2 hover:cursor-pointer hover:bg-poimandres-darkslate"
+        className="flex h-10 w-full flex-row items-center border border-poimandres-midslate pl-2 hover:cursor-pointer hover:bg-poimandres-darkslate"
         onClick={handleClick}
       >
         {expanded ? (
@@ -25,11 +26,19 @@ const ExpandablePanel = ({ header, children }: ExpandablePanelProps) => {
         )}
         {header}
       </div>
-      {expanded && (
-        <div className="border-t border-poimandres-midslate p-2">
+      <Transition
+        show={expanded}
+        enter="transition-opacity duration-500"
+        enterFrom="opacity-0"
+        enterTo="opacity-100"
+        leave="transition-opacity duration-500"
+        leaveFrom="opacity-100"
+        leaveTo="opacity-0"
+      >
+        <div className="border border-t-0 border-poimandres-midslate p-2">
           {children}
         </div>
-      )}
+      </Transition>
     </div>
   );
 };
