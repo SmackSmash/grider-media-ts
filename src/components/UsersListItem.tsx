ad2deff3beacf440defefecfc6e0ea3598f50fc5
Dan Moffat <danmoffat@gmail.com>
2024-05-01 01:57:07 +0100

    Fixed mouse event on Button component

--- src/components/UsersListItem.tsx
+++ src/components/UsersListItem.tsx
@@ -1,6 +1,7 @@
 import Button from './Button';
 import useThunk from '../hooks/useThunk';
 import { deleteUser } from '../store';
+import { type MouseEvent } from 'react';
 
 interface UsersListItemProps {
   name: string;
@@ -11,14 +12,24 @@ const UsersListItem = ({ name, id }: UsersListItemProps) => {
   const [doDeleteUser, isDeletingUser, deletingUserError] =
     useThunk(deleteUser);
 
-  const handleDelete = (id: string) => {
+  const handleDelete = (id: string, e: MouseEvent<HTMLButtonElement>) => {
+    e.stopPropagation();
     doDeleteUser(id);
   };
 
+  const handleClick = () => {
+    console.log('clicked');
+  };
+
   return (
-    <div className='flex h-10 w-full pl-2 my-2 items-center bg-poimandres-darkslate justify-between hover:bg-poimandres-darkerblue hover:cursor-pointer'>
+    <div
+      className='flex h-10 w-full pl-2 my-2 items-center justify-between hover:bg-poimandres-darkslate hover:cursor-pointer'
+      onClick={handleClick}>
       {name}
-      <Button danger loading={isDeletingUser} onClick={() => handleDelete(id)}>
+      <Button
+        danger
+        loading={isDeletingUser}
+        onClick={e => handleDelete(id, e)}>
         {deletingUserError ? 'Oopsy!' : 'Delete'}
       </Button>
     </div>
