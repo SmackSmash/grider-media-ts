5e0092651e68a5c48a7b5f5d2049ecb8f82b03c1
Dan Moffat <danmoffat@gmail.com>
2024-05-01 19:36:42 +0100

    Naive implementation of query routed in api

--- src/store/apis/albumsApi.ts
+++ src/store/apis/albumsApi.ts
@@ -1,4 +1,4 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 interface Album {
   id: string;
@@ -7,13 +7,19 @@ interface Album {
 }
 
 const albumsApi = createApi({
-  reducerPath: 'albums',
-  baseQuery: fetchBaseQuery({ baseUrl: 'http://localhost:3000/' }),
-  endpoints: builder => ({
+  reducerPath: "albums",
+  baseQuery: fetchBaseQuery({ baseUrl: "http://localhost:3000/" }),
+  endpoints: (builder) => ({
     getAlbumsByUserId: builder.query<Album, string>({
-      query: userId => `albums?userId=${userId}`
-    })
-  })
+      query: (userId) => `albums?userId=${userId}`,
+    }),
+    createAlbum: builder.query<Album, string>({
+      query: () => "albums",
+    }),
+    deleteAlbum: builder.query<Album, string>({
+      query: (id) => `albums/${id}`,
+    }),
+  }),
 });
 
 export default albumsApi;
