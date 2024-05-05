d1bb12698098eb6a6fceb9c4c9466bcc58fdfd72
Dan Moffat <danmoffat@gmail.com>
2024-05-05 21:25:55 +0100

    Moved to live db and removed delay functions

--- src/store/thunks/fetchUsers.ts
+++ src/store/thunks/fetchUsers.ts
@@ -1,9 +1,8 @@
 import { createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
-import { delay } from '../../utils';
 
 export const fetchUsers = createAsyncThunk('users/fetch', async () => {
   const response = await axios.get('http://localhost:3000/users');
-  await delay(500);
+  console.log(response);
   return response.data;
 });
