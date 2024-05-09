95c3ab3e163d0296deedaa8df77cea978740eb15
Dan Moffat <danmoffat@gmail.com>
2024-05-09 01:07:17 +0100

    Implemented hook to fetch user albums, TS isn't happy though

--- src/components/AlbumsList.tsx
+++ src/components/AlbumsList.tsx
@@ -1,9 +1,20 @@
+import { useGetAlbumsByUserIdQuery } from '../store';
 interface AlbumsListProps {
   id: string;
 }
 
 const AlbumsList = ({ id }: AlbumsListProps) => {
-  return <div>Albums for user with id {id}</div>;
+  const { data, error, isLoading } = useGetAlbumsByUserIdQuery(id);
+
+  if (isLoading) {
+    return <div>Loading albums...</div>;
+  }
+
+  if (error) {
+    return <div>Something went wrong</div>;
+  }
+
+  return <div>{data.length ? data.map(({ title }) => <p>{title}</p>) : 'No albums found'}</div>;
 };
 
 export default AlbumsList;
