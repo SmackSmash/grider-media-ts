ee8d3e22a0e34a0f34ff2ec0fe420a907a50b5d5
Dan Moffat <danmoffat@gmail.com>
2024-05-09 01:40:08 +0100

    A little styling

--- src/components/UsersListItem.tsx
+++ src/components/UsersListItem.tsx
@@ -22,7 +22,7 @@ const UsersListItem = ({ name, id, email, image }: UsersListItemProps) => {
 
   const header = (
     <>
-      <div className='mr-2 flex h-8 w-8 items-center justify-center overflow-hidden rounded-full'>
+      <div className='mr-2 flex h-8 w-8 items-center justify-center overflow-hidden rounded-full border-2 border-poimandres-midslate'>
         <img
           className='h-full w-full object-cover'
           src={`http://localhost:3000/images/profile/${image}.jpg`}
