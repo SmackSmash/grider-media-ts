192853aa95b558cdd0c38af8696664431f29e49a
Dan Moffat <danmoffat@gmail.com>
2024-05-09 01:58:40 +0100

    Better graphical loading state with skeleton

--- src/components/AlbumsList.tsx
+++ src/components/AlbumsList.tsx
@@ -1,4 +1,5 @@
 import { useGetAlbumsByUserIdQuery, useCreateAlbumForUserMutation } from '../store';
+import Skeleton from './Skeleton';
 import ExpandablePanel from './ExpandablePanel';
 import Button from './Button';
 interface AlbumsListProps {
@@ -13,22 +14,36 @@ const AlbumsList = ({ userId }: AlbumsListProps) => {
     createAlbum(userId);
   };
 
+  const albumsHeader = (
+    <div className='mb-4 mt-2 flex items-center'>
+      <h2>Albums</h2>
+      <Button className='ml-auto mr-2' success onClick={handleClick}>
+        + Add Album
+      </Button>
+    </div>
+  );
+
   if (isLoading) {
-    return <div>Loading albums...</div>;
+    return (
+      <>
+        {albumsHeader}
+        <Skeleton times={3} className='mb-4 mt-2 h-10 w-full' />;
+      </>
+    );
   }
 
   if (error) {
-    return <div>Something went wrong</div>;
+    return (
+      <>
+        {albumsHeader}
+        <div>Something went wrong</div>;
+      </>
+    );
   }
 
   return (
     <div>
-      <div className='mb-4 mt-2 flex items-center'>
-        <h2>Albums</h2>
-        <Button className='ml-auto mr-2' success onClick={handleClick}>
-          + Add Album
-        </Button>
-      </div>
+      {albumsHeader}
       {data!.length
         ? data!.map(({ title, _id }) => (
             <ExpandablePanel header={<h3>{title}</h3>} key={_id}>
