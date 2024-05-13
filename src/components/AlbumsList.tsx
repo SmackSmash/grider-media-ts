55d00dcd47cb18d6312c2a73b424adb21727c852
Dan Moffat <danmoffat@gmail.com>
2024-05-13 23:52:09 +0100

    Added client side logic to handle album deletion

--- src/components/AlbumsList.tsx
+++ src/components/AlbumsList.tsx
@@ -10,15 +10,38 @@ const AlbumsList = ({ userId }: AlbumsListProps) => {
   const { data, isError, isLoading } = useGetAlbumsByUserIdQuery(userId);
   const [createAlbum, results] = useCreateAlbumForUserMutation();
 
-  const handleClick = async () => {
+  const handleCreateAlbum = async () => {
     await createAlbum(userId);
+  };
+
+  const handleDeleteAlbum = (albumId: string) => {
+    console.log(`Delete album with ID of ${albumId}`);
   };
 
   const albumsHeader = (
     <div className='mb-4 mt-2 flex items-center'>
       <h2>Albums</h2>
-      <Button className='ml-auto mr-2' success loading={results.isLoading} onClick={handleClick}>
+      <Button
+        className='ml-auto mr-2'
+        success
+        loading={results.isLoading}
+        onClick={handleCreateAlbum}
+      >
         + Add Album
+      </Button>
+    </div>
+  );
+
+  const albumHeader = (title: string, albumId: string) => (
+    <div className='flex w-full items-center'>
+      <h3>{title}</h3>
+      <Button
+        className='ml-auto mr-2'
+        warning
+        loading={results.isLoading}
+        onClick={() => handleDeleteAlbum(albumId)}
+      >
+        + Delete Album
       </Button>
     </div>
   );
@@ -46,7 +69,7 @@ const AlbumsList = ({ userId }: AlbumsListProps) => {
       {albumsHeader}
       {data!.length
         ? data!.map(({ title, _id }) => (
-            <ExpandablePanel header={<h3>{title}</h3>} key={_id}>
+            <ExpandablePanel header={albumHeader(title, _id)} key={_id}>
               {title}
             </ExpandablePanel>
           ))
